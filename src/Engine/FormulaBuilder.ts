c0c36ee586c7d13f316944524f806cb67a612895
buhuiyonggou <yangli4944@gmail.com>
2023-11-10 19:39:25 -0800

    bug fix with limiting token numbers, need to be modified

--- src/Engine/FormulaBuilder.ts
+++ src/Engine/FormulaBuilder.ts
@@ -1,33 +1,36 @@
 /**
  *  class TokenProcessor
- * 
+ *
  * This class is responsible for processing the tokens
- * 
+ *
  * a local Formula is used to store the current formula
- * 
+ *
  * It provides a method to add a token to the formula
- * 
+ *
  * It provides a method to get the formula
- * 
- * It provides a method to get the display string 
- * 
+ *
+ * It provides a method to get the display string
+ *
  */
 import Cell from "./Cell";
 
 export class FormulaBuilder {
-
   // the current formula
   private formula: FormulaType = [];
+  // define the max length of the formula
+  private readonly maxFormulaLength: number = 25;
+  private readonly maxNonNumericTokens: number = 5;
+  private _lastErrorMessage: string = "";
 
   constructor() {
     this.formula = [];
   }
 
   /**
-   * 
-   * @param 
+   *
+   * @param
    * @returns the value of the formula
-   * 
+   *
    * */
   getFormula(): FormulaType {
     return this.formula;
@@ -37,8 +40,29 @@ export class FormulaBuilder {
    *  Set the formula
    */
   setFormula(formula: FormulaType): void {
-
     this.formula = [...formula];
+    if (formula.length === 0) {
+      this._lastErrorMessage = ""; // Reset error message when formula is cleared
+    }
+  }
+
+  private formulaLengthWithNewToken(newToken: TokenType): number {
+    // This needs to be implemented according to how you calculate the length
+    // For example, if each token is a string, you can just sum up the lengths
+    return this.formula.reduce(
+      (length, token) => length + token.length,
+      newToken.length
+    );
+  }
+
+  // Method to count non-numeric tokens in the formula
+  private countNonNumericTokens(): number {
+    return this.formula.filter((token) => isNaN(Number(token))).length;
+  }
+
+  // get error message
+  public getLastError(): string {
+    return this._lastErrorMessage;
   }
 
   /**
@@ -56,6 +80,22 @@ export class FormulaBuilder {
   addToken(token: TokenType): void {
     let lastTokenUpdated = false;
     let ignoringToken = false;
+    // Check non-numeric token count
+    if (
+      isNaN(Number(token)) &&
+      this.countNonNumericTokens() >= this.maxNonNumericTokens
+    ) {
+      this._lastErrorMessage = "Too many non-numeric tokens.";
+      return;
+    }
+    // Check if adding the new token exceeds the maximum formula length
+    if (this.formulaLengthWithNewToken(token) > this.maxFormulaLength) {
+      // Here you can handle the scenario when the formula is too long
+      // For example, you can ignore the new token or display an error message
+      this._lastErrorMessage = "Formula is too long.";
+      return;
+    }
+
     // if there is no formula then add the token to the formula
     if (this.formula.length === 0) {
       this.formula = [...this.formula, token];
@@ -76,41 +116,42 @@ export class FormulaBuilder {
       ignoringToken = true;
     }
 
-    // check for the existence of a period in the previous token first 
+    // check for the existence of a period in the previous token first
     // if the last token is a number and the input token is . and the number contains a . then do not append the input token to the last token
     // if the last token contains a "."
     if (!isNaN(Number(lastToken)) && token === "." && lastToken.includes(".")) {
       lastTokenUpdated = false;
-      ignoringToken = true
+      ignoringToken = true;
     }
 
     // if the last token is a number and the input token is . then append the input token to the last token
     // if the last token does not contain a "."
-    if (!isNaN(Number(lastToken)) && token === "." && !lastToken.includes(".")) {
+    if (
+      !isNaN(Number(lastToken)) &&
+      token === "." &&
+      !lastToken.includes(".")
+    ) {
       lastToken += token;
       lastTokenUpdated = true;
     }
-
 
     // If we updated the last token then replace the last token in the formula with the updated token
     // if the ignoringToken flag is set to true then do not update the last token
     if (lastTokenUpdated) {
       this.formula[this.formula.length - 1] = lastToken;
-    }
-    else if (!ignoringToken) {// add the token to the formula
+    } else if (!ignoringToken) {
+      // add the token to the formula
       this.formula = [...this.formula, token];
-    }
-    else {
+    } else {
       // do nothing but leave the else here for clarity
       // this is where we would handle the case where we are ignoring the token
       // fortunately we do not need to do anything here
     }
-
   }
 
   /**
    * remove the last token from the formula
-   * 
+   *
    * if the last token is a number with more than one character it should only remove the last character of that token
    */
   removeToken(): void {
@@ -126,16 +167,18 @@ export class FormulaBuilder {
     if (!isNaN(Number(lastToken)) && lastToken.length > 1) {
       lastToken = lastToken.substring(0, lastToken.length - 1);
       this.formula[this.formula.length - 1] = lastToken;
-    }
-    else {
+    } else {
       // remove the last token from the formula
       this.formula.pop();
     }
+    // Re-evaluate conditions and reset error message if conditions are no longer violated
+    if (
+      this.formula.length <= this.maxFormulaLength &&
+      this.countNonNumericTokens() <= this.maxNonNumericTokens
+    ) {
+      this._lastErrorMessage = "";
+    }
   }
-
-
-
-
 
   /**
    * getFormulaString
@@ -144,7 +187,6 @@ export class FormulaBuilder {
    * remove the last space if there is one
    */
   getFormulaString(): string {
-
     let result = "";
 
     for (let i = 0; i < this.formula.length; i++) {
@@ -154,14 +196,14 @@ export class FormulaBuilder {
     if (result.length > 0) {
       result = result.substring(0, result.length - 1);
     }
-    return result;
 
+    return result;
   }
 
   /**
    * parse the formula and return a list of cell references (deduped)
-   * 
-   * @returns a list of cell references 
+   *
+   * @returns a list of cell references
    * */
   public static getCellReferences(formula: string[]): string[] {
     let result: string[] = [];
@@ -177,10 +219,6 @@ export class FormulaBuilder {
 
     return result;
   }
-
 }
 
 export default FormulaBuilder;
-
-
-
